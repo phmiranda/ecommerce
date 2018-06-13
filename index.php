3054cb033a59ff19630d2d25a20abb63ca13dbac
pedro.miranda <pehhagah.1607@gmail.com>
2018-06-13 16:45:06 -0300

    refatoração do nome do arquivo de configurações do cabeçalho e rodapé.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php require_once('front-header.php'); ?>
+<?php require_once('header.php'); ?>
 <div class="container">
     <div class="row">
         <h3 class="text-center text-success"> Bem Vindo ao Sistema de Controle de Estoque da Alura. </h3>
     </div>
 </div>
-<?php require_once('front-footer.php'); ?>
+<?php require_once('footer.php'); ?>
