9b5579f6f142374ce356edd2f2276cd1ad3d86d2
pedro.miranda <pehhagah.1607@gmail.com>
2018-07-08 13:49:02 -0300

    criação do arquivo de cadastro dos produtos e usuários.

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -4,6 +4,7 @@ require_once('header.php');
 
 // comentário
 $nome = $_POST['nome'];
+$categoria = $_POST['categoria_id'];
 $preco = $_POST['preco'];
 
 echo "<p class='text-success principal'>O produto {$nome} com o valor de R$ {$preco} foi adicionado a nossa base de dados com sucesso.</p>";
---
+++ adiciona-usuario.php
@@ -0,0 +1,11 @@
+<?php
+// comentário
+require_once('header.php');
+
+// comentário
+$nome = $_POST['nome'];
+
+echo "<p class='text-success principal'>O usuário {$nome} foi cadastrado com sucesso.</p>";
+
+// comentário
+require_once('footer.php');
