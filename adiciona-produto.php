37bb48b8677713c6aea0602761c9b8d004a9f8d3
pedro.miranda <pehhagah.1607@gmail.com>
2018-07-11 18:48:59 -0300

    inclusão da conexão com o banco de dados e query de inserção de registros na base de dados.

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -4,10 +4,15 @@ require_once('header.php');
 
 // comentário
 $nome = $_POST['nome'];
-$categoria = $_POST['categoria_id'];
 $preco = $_POST['preco'];
+$descricao = $_POST['descricao'];
+$categoria_id = $_POST['categoria_id'];
 
-echo "<p class='text-success principal'>O produto {$nome} com o valor de R$ {$preco} foi adicionado a nossa base de dados com sucesso.</p>";
+// comentários
+$conn = mysqli_connect('localhost','root','root', 'db_ecommerce');
+$query = "INSERT INTO produtos(nome, preco, descricao, categoria_id) VALUES({$nome},{$preco},{$descricao},{$categoria_id})";
+
+echo "<p class='alert-success principal'>O produto {$nome} foi adicionado a nossa base de dados com sucesso.</p>";
 
 // comentário
 require_once('footer.php');
