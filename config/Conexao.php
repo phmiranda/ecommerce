4b0b7a44d786e9e4ee910b1a12c05c9b32d6ee59
pedro.miranda <pehhagah.1607@gmail.com>
2018-07-20 12:12:59 -0300

    retirado a validação do ambiente na hora de realizar a conexão com o banco de dados.

--- config/Conexao.php
+++ config/Conexao.php
@@ -3,11 +3,16 @@
 class Conexao {
     // comentários
     public function conectar(){
+        // faz a leitura do .ini com as configurações do banco de dados
 
+        // verifica qual o ambiente deverá ser conectado no banco de dados.
+
+        // faz a conexão com a base de dados do determinado ambiente.
     }
 
     // comentários
     public function desconectar(){
+        // faz a desconexão da base de dados.
 
     }
 }
