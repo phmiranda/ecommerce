dfafd1980b1654780a22a5b6c32c4a75c19a5ca4
pedro.miranda <pedro.miranda@basis.com.br>
2018-07-24 00:50:26 -0300

    criação da camada de serviços para validação de regras de negócio durante a criação e execução dos métodos na model ou controllers.

---
+++ services/CategoriaService.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pedro
+ * Date: 24/07/2018
+ * Time: 00:49
+ */
+
+class CategoriaService
+{
+
+}
---
+++ services/UsuarioService.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pedro
+ * Date: 24/07/2018
+ * Time: 00:48
+ */
+
+class UsuarioService
+{
+
+}
