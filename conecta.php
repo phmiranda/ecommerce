b1ac9c98dc7bb6f994be3a646c15371371e5508e
pedro.miranda <pehhagah.1607@gmail.com>
2018-08-02 14:33:17 -0300

    atualização no nome da base de dados usada como referência.

--- conecta.php
+++ conecta.php
@@ -2,9 +2,9 @@
 
 // variáveis de conexão com a base de dados
 $server = 'localhost';
-$database = 'db_ecommerce_alura';
-$username = 'root';
-$password = 'root';
+$database = 'db_ecommerce';
+$username = 'admin';
+$password = 'admin';
 
 // script de conexão com o banco de dados.
 $conn = mysqli_connect($server,$username,$password, $database);
