6acce7b9d8ddf28301dd8b39e50f60d5a6f6f3b7
pedro.miranda <pedro.miranda@terracap.df.gov.br>
2018-10-08 10:11:14 -0300

    criação da automatização de atualização dos repositórios.

--- ecommerce.sh
+++ ecommerce.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 # comentários
-/bin/bash
+echo "atualizando os repositórios do sistema operacional..."
+if ! sudo apt-get update -y
+then
+    echo "falha ao atualizar os repositórios..."
+    exit
+fi
+    echo "os repositórios foram atualizados com sucesso..."
