686c9d806ae0f5e0167f37d29d225b5fff16554b
pedro.miranda <pedro.miranda@terracap.df.gov.br>
2018-10-08 15:21:28 -0300

    criação da tabela de situações e inclusão da chave estrangeira na tabela produtos.

--- assets/db_ecommerce.sql
+++ assets/db_ecommerce.sql
@@ -1,4 +1,11 @@
-/* comentário */
+/* @usuarios */
+CREATE TABLE perfis(
+  id_perfil INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  nome VARCHAR() NOT NULL ,
+  descricao LONGTEXT NULL DEFAULT "N/A"
+);
+
+/* @usuarios */
 CREATE TABLE usuarios(
   id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(255) NOT NULL ,
@@ -6,20 +13,29 @@ CREATE TABLE usuarios(
   senha VARCHAR (255) NOT NULL 
 );
 
-/* comentários */
+/* @categorias */
 CREATE TABLE categorias(
   id_categoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(50) NOT NULL ,
-  descricao LONGTEXT NULL
+  descricao LONGTEXT NULL DEFAULT "N/A"
 );
 
-/* comentários */
+/* @situacao */
+CREATE TABLE situacao (
+  id_situacao INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  nome_situacao VARCHAR(15) NOT NULL ,
+  descricao LONGTEXT NULL DEFAULT "N/A"
+);
+
+/* @produtos */
 CREATE TABLE produtos(
   id_produto INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(100) NOT NULL ,
   preco DECIMAL (10,2) NOT NULL ,
-  situacao INT NOT NULL DEFAULT 0 ,
-  descricao LONGTEXT NULL ,
+  descricao LONGTEXT NULL DEFAULT "N/A",
   categoria_id INT NOT NULL,
-  FOREIGN KEY(categoria_id) REFERENCES categorias(id_categoria)
+  situacao_id INT NOT NULL DEFAULT 1 ,
+  FOREIGN KEY(categoria_id) REFERENCES categorias(id_categoria),
+  FOREIGN KEY(situacao_id) REFERENCES situacao(id_situacao)
 );
+
