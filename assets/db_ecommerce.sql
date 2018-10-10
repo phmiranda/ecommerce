c521d9cd33a75f8d1324d185e2170925233ba40a
pedro.miranda <pedro.miranda@terracap.df.gov.br>
2018-10-10 14:25:03 -0300

    atualização no script de criação das tabelas do banco de dados

--- assets/db_ecommerce.sql
+++ assets/db_ecommerce.sql
@@ -1,8 +1,8 @@
 /* @usuarios */
 CREATE TABLE perfis(
   id_perfil INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  nome VARCHAR() NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT "N/A"
+  nome VARCHAR(30) NOT NULL ,
+  descricao LONGTEXT NULL DEFAULT 'N/A'
 );
 
 /* @usuarios */
@@ -10,21 +10,23 @@ CREATE TABLE usuarios(
   id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(255) NOT NULL ,
   email VARCHAR(100) NOT NULL ,
-  senha VARCHAR (255) NOT NULL 
+  senha VARCHAR (255) NOT NULL ,
+  perfil_id INT NOT NULL ,
+  FOREIGN KEY (perfil_id) REFERENCES perfis(id_perfil)
 );
 
 /* @categorias */
 CREATE TABLE categorias(
   id_categoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(50) NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT "N/A"
+  descricao LONGTEXT NULL DEFAULT 'N/A'
 );
 
 /* @situacao */
 CREATE TABLE situacao (
   id_situacao INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  nome_situacao VARCHAR(15) NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT "N/A"
+  nome VARCHAR(15) NOT NULL ,
+  descricao LONGTEXT NULL DEFAULT 'N/A'
 );
 
 /* @produtos */
@@ -32,10 +34,9 @@ CREATE TABLE produtos(
   id_produto INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(100) NOT NULL ,
   preco DECIMAL (10,2) NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT "N/A",
+  descricao LONGTEXT NULL DEFAULT 'N/A',
   categoria_id INT NOT NULL,
   situacao_id INT NOT NULL DEFAULT 1 ,
   FOREIGN KEY(categoria_id) REFERENCES categorias(id_categoria),
   FOREIGN KEY(situacao_id) REFERENCES situacao(id_situacao)
 );
-
