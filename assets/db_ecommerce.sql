186151c019e23a18a05b2504931c17a5f3746b2e
pedro.miranda <pehhagah.1607@gmail.com>
2018-10-20 00:41:41 -0300

    atualização no script de criação das tabelas na base de dados, foram retirados a síntaxe 'DEFAULT',já que a mesma impede a criação das tabelas.

--- assets/db_ecommerce.sql
+++ assets/db_ecommerce.sql
@@ -2,7 +2,7 @@
 CREATE TABLE perfis(
   id_perfil INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(30) NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT 'N/A'
+  descricao LONGTEXT NULL
 );
 
 /* @usuarios */
@@ -19,14 +19,14 @@ CREATE TABLE usuarios(
 CREATE TABLE categorias(
   id_categoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(50) NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT 'N/A'
+  descricao LONGTEXT NULL
 );
 
 /* @situacao */
 CREATE TABLE situacao (
   id_situacao INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(15) NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT 'N/A'
+  descricao LONGTEXT NULL
 );
 
 /* @produtos */
@@ -34,7 +34,7 @@ CREATE TABLE produtos(
   id_produto INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   nome VARCHAR(100) NOT NULL ,
   preco DECIMAL (10,2) NOT NULL ,
-  descricao LONGTEXT NULL DEFAULT 'N/A',
+  descricao LONGTEXT NULL,
   categoria_id INT NOT NULL,
   situacao_id INT NOT NULL DEFAULT 1 ,
   FOREIGN KEY(categoria_id) REFERENCES categorias(id_categoria),
