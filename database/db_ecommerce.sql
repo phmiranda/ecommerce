2a20082a262eb7f24c1ee3bacb5cd775f115fc06
pedro.miranda <pedro.miranda@capgemini.com>
2019-02-07 09:02:44 -0200

    correção no script para execução correta via mysqldump

--- database/db_ecommerce.sql
+++ database/db_ecommerce.sql
@@ -1,29 +1,29 @@
 /* comentários */
-CREATE TABLE `categorias` (
-  id_categoria int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  nome varchar(30) NOT NULL
+CREATE TABLE categorias (
+  id_categoria INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  nome VARCHAR(30) NOT NULL
 );
 
 /* comentários */
-CREATE TABLE `produtos` (
-  id_produto int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  nome varchar(100) NOT NULL,
-  preco decimal(10,2) NOT NULL,
+CREATE TABLE produtos (
+  id_produto INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  nome VARCHAR(100) NOT NULL,
+  preco DECIMAL(10,2) NOT NULL,
   descricao LONGTEXT NULL ,
-  categoria_id int(11) NOT NULL,
-  situacao_id int(11) NOT NULL
+  categoria_id INT(11) NOT NULL,
+  situacao_id INT(11) NOT NULL
 );
 
 /* comentários */
 CREATE TABLE situacao (
-  id_situacao int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  nome varchar(30) NOT NULL
+  id_situacao INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  nome VARCHAR(30) NOT NULL
 );
 
 /* comentários */
 CREATE TABLE usuarios (
-  id_usuario int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  nome varchar(255) NOT NULL,
-  email varchar(100) NOT NULL,
-  senha varchar(255) NOT NULL
+  id_usuario INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  senha VARCHAR(255) NOT NULL
 );
